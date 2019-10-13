e9429902b8abc5caf4a17055fbeca9313c702e4b
Mike Fleck <fleckguitarrepair@gmail.com>
2019-10-13 09:09:07 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import loopToFile
-import loop
-import _lines
+# from auto_G.print_to_file import loopToFile
+from auto_G.consoleCode.box import loop
+# from auto_G.gui import _lines
 
-_l = loop.Boxing()
-_L2f = loopToFile.BoxingToFile()
-_lines = _lines.__lines()
+_l = loop.Boxing
+_l()
+# _L2f = loopToFile.BoxingToFile()
+# _lines = _lines.__lines()
